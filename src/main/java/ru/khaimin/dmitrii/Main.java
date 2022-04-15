123279864ac0c0663ab5e504ca3fe8bc3b1c8895
Dmitrii Khaimin <khaimindm@gmail.com>
2022-04-15 19:33:25 +0400

    15.04.2022

---
+++ src/main/java/ru/khaimin/dmitrii/Main.java
@@ -0,0 +1,22 @@
+package ru.khaimin.dmitrii;
+
+import org.apache.commons.text.WordUtils;
+
+/**
+ * Hello world!
+ *
+ */
+
+/**
+ * Текст для документации
+ @author Dmitrii
+ @version 1.0
+ */
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( WordUtils.capitalize("i am FINE"));
+    }
+}
---
+++ src/test/java/ru/khaimin/dmitrii/AppTest.java
@@ -0,0 +1,20 @@
+package ru.khaimin.dmitrii;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
